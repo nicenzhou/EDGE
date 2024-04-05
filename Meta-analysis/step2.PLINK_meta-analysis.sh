ded8f40810b0931f18faa96fe9252b786f9399bf
Jiayan Zhou <43587078+nicenzhou@users.noreply.github.com>
2024-04-04 22:49:47 -0700

    Update step2.PLINK_meta-analysis.sh

--- Meta-analysis/step2.PLINK_meta-analysis.sh
+++ Meta-analysis/step2.PLINK_meta-analysis.sh
@@ -1,1 +1,5 @@
+./plink --meta-analysis ukb_edge_gwas_cad_eur_meta.txt mvp_edge_gwas_cad_eur_meta.txt --out meta_analysis_edge_cad_eur;
+./plink --meta-analysis ukb_edge_gwas_bmi_eur_meta.txt mvp_edge_gwas_bmi_eur_meta.txt + qt --out meta_analysis_edge_bmi_eur;
 
+./plink --meta-analysis ukb_plink_gwas_cad_eur_meta.txt mvp_plink_gwas_cad_eur_meta.txt --out meta_analysis_plink_cad_eur;
+./plink --meta-analysis ukb_plink_gwas_bmi_eur_meta.txt mvp_plink_gwas_bmi_eur_meta.txt + qt --out meta_analysis_plink_bmi_eur;
